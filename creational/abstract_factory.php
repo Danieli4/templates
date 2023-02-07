d0a4c5348197dbfc02e41593afa866fd803d8eec
danilgryaznev <daniel58912007@gmail.com>
2023-02-07 22:06:58 +0300

    1

---
+++ creational/abstract_factory.php
@@ -0,0 +1,89 @@
+<?php
+interface AbstractFactory
+{
+    public static function makeDeveloperWorker():DeveloperWorker;
+    public static function makeDesignWorker():DesignWorker;
+}
+
+class OutsourceWorkerFactory implements AbstractFactory
+{
+
+    public static function makeDesignWorker(): DesignWorker
+    {
+       return new OutsourceDesignWorker();
+    }
+
+    public static function makeDeveloperWorker(): DeveloperWorker
+    {
+        return new OutsourceDeveloperWorker();
+    }
+}
+
+class NativeWorkerFactory implements AbstractFactory
+{
+
+    public static function makeDeveloperWorker(): DeveloperWorker
+    {
+        return new NativeDeveloperWorker();
+    }
+
+    public static function makeDesignWorker(): DesignWorker
+    {
+        return new NativeDesignWorker();
+    }
+}
+
+interface Worker
+{
+    public function work();
+    
+}
+
+interface DeveloperWorker extends Worker
+{
+    
+}
+
+interface DesignWorker extends Worker
+{
+    
+}
+
+class NativeDeveloperWorker implements DeveloperWorker
+{
+    public function work()
+    {
+        printf('im developing as native');
+    }
+}
+
+class OutsourceDeveloperWorker implements DeveloperWorker
+{
+    public function work()
+    {
+        printf('im developing as outsource');
+    }
+}
+
+class NativeDesignWorker implements DesignWorker
+{
+    public function work()
+    {
+        printf('im design as native');
+    }
+}
+
+class OutsourceDesignWorker implements DesignWorker
+{
+    public function work()
+    {
+        printf('im design as outsource');
+    }
+}
+
+$nativeDeveloper = NativeWorkerFactory::makeDeveloperWorker();
+$outsourceDeveloper = OutsourceWorkerFactory::makeDeveloperWorker();
+$nativeDesigner = NativeWorkerFactory::makeDesignWorker();
+$outsourceDesigner = OutsourceWorkerFactory::makeDesignWorker();
+
+$outsourceDesigner->work();
---
+++ creational/factory_method.php
@@ -0,0 +1,54 @@
+<?php
+//
+//interface Worker
+//{
+//    public function work();
+//}
+//
+//class Developer implements Worker
+//{
+//    public function work()
+//    {
+//        printf("im developer");
+//
+//    }
+//
+//}
+//
+//class Designer implements Worker
+//{
+//    public function work()
+//    {
+//        printf("im design");
+//    }
+//
+//}
+//
+//interface WorkerFactory
+//{
+//    public static function make();
+//}
+//
+//class DeveloperFactory implements WorkerFactory
+//{
+//
+//    public static function make()
+//    {
+//        return new Developer();
+//    }
+//}
+//
+//class DesignerFactory implements WorkerFactory
+//{
+//
+//    public static function make()
+//    {
+//        return new Designer();
+//    }
+//}
+//
+//$developer = DeveloperFactory::make();
+//$design = DesignerFactory::make();
+//
+//$developer->work();
+
